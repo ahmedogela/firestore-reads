4143469503a827ec03ce3586af5666582d16709e
Ahmed Ogela <ogela@appatlab.com>
2021-01-02 09:19:33 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'firestore-reads';
+export class AppComponent implements OnInit, OnDestroy {
+  items = [];
+  itemsSubscription: Subscription;
+  constructor(private afs: AngularFirestore) { }
+
+  ngOnInit() {
+    const itemsCollection: AngularFirestoreCollection = this.afs.collection('Items');
+    this.itemsSubscription = itemsCollection.snapshotChanges()
+      .pipe(
+        map((arr) => {
+          return arr.map((snap) => {
+            if (snap.payload.doc.data()) {
+              return {
+                ...snap.payload.doc.data(),
+                id: snap.payload.doc.id,
+                fromCache: snap.payload.doc.metadata.fromCache,
+              };
+            } else {
+              return snap.payload.doc.data();
+            }
+          });
+        })
+      )
+      .subscribe(items => {
+        this.items = items;
+        /// it emits twice when persistence enabled
+        /// first one from cache the from server
+        /// this counts as extra reads
+        /// why it comes from server and counts as reads while there is no update to the database.
+        console.log(items);
+      })
+  }
+
+  addItem() {
+    const title = 'Item: ' + this.getRandom();
+    this.afs.collection('Items').add({ title })
+  }
+
+  getRandom() {
+    return Math.round(Math.random() * (999999 - 9999) + 9999);
+  }
+
+  ngOnDestroy() {
+    if (this.itemsSubscription) {
+      this.itemsSubscription.unsubscribe();
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { AppComponent } from './app.component';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAAC4zZ2PyhJiV77585ZJ_arqQhkOnc40Y",
+  authDomain: "ng-test-930a5.firebaseapp.com",
+  databaseURL: "https://ng-test-930a5.firebaseio.com",
+  projectId: "ng-test-930a5",
+  storageBucket: "ng-test-930a5.appspot.com",
+  messagingSenderId: "12354345260",
+  appId: "1:12354345260:web:ed680e9c4bb5e8d9c87489"
+};
+// Initialize Firebase
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule.enablePersistence(),
   ],
   providers: [],
   bootstrap: [AppComponent]
